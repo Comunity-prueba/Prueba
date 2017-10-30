ea09740b5f4523aa160accfa5a5c936ab63f8d18
Nihao <erickjhorman@hotmail.com>
2017-10-30 16:31:42 -0500

    Nuevas variables

--- funciones.js
+++ funciones.js
@@ -9,6 +9,16 @@ var num2= form1.n2.value;
     var num2= form.n8.value;
     var num1= form1.n1.value;
 var num2= form1.n2.value;
+        var num1= form1.n1.value;
+var num2= form1.n2.value;
+    var num2= form.n8.value;
+    var num1= form1.n1.value;
+var num2= form1.n2.value;
+        var num1= form1.n1.value;
+var num2= form1.n2.value;
+    var num2= form.n8.value;
+    var num1= form1.n1.value;
+var num2= form1.n2.value;
     
 var resultado= parseInt(num1) + parseInt(num2);
 alert("El resultado de la suma es: "+ resultado);
